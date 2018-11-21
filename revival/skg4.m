5b5a27d6f4381cd50c849a34c445acb3faacebb8
ptrktn <ptrktn@users.noreply.github.com>
2018-11-21 18:57:39 +0900

    SKG4 base

---
+++ revival/skg4.m
@@ -0,0 +1,224 @@
+#! /usr/bin/octave -qf
+# -*-octave-*-
+# SKG model DOI: 10.1021/jp9832721
+# Based on Eqs E6-E8 and simulate the dynamics by varying initial [BrO3-]
+# and [BrCHD], both of which decay as a function of time.
+#
+# Additionally this implementation includes linear decay of [BrO3-].
+# [BrO3-] = [BrO3-]_0 - T * t;
+
+global galpha = 1000000;
+global gbeta = 77.4;
+global ggamma = 1102;
+global gdelta = 2719;
+global gf = 0.776;
+global BrO30 = 0.2;
+global BrCHD0 = 0.022;
+global kBrO3 = 0;
+global kBrCHD = 0;
+global k2 =  2500000;
+global k14 =  5.0000e-05;
+global k17 =  0.02;
+global BrCHD = 0.02;
+global kdp = 280; # kd'
+global k5 = 170000;
+global k4 = 2000000;
+global k4r = 100000000; # k_{-4}
+global H = 1.29;
+
+arg_list = argv();
+
+# Command-line arguments are mandatory
+if (4 == nargin)
+  BrO30 = str2num(arg_list{1});
+  BrCHD0 = str2num(arg_list{2});
+  kBrO3 = str2num(arg_list{3});
+  kBrCHD = str2num(arg_list{4});
+else
+  quit(1);
+endif
+
+printf("BrO30 = %f, BrCHD0 = %f, kBrO3 = %f, kBrCHD = %f\n",
+	   BrO30, BrCHD0, kBrO3, kBrCHD);
+
+# Eqs E6-E8
+function xdot = f (x, t)
+  global galpha;
+  global gbeta;
+  global ggamma;
+  global gdelta;
+  global gf;
+  global k2;
+  global k14;
+  global k17;
+  global kdp;
+  global k5;
+  global k4;
+  global k4r;
+  global BrCHD;
+  global T;
+  global BrO30;
+  global BrCHD0;
+  global kBrO3;
+  global kBrCHD;
+  global H;
+
+  BrO3 = BrO30 - kBrO3 * t;
+  BrCHD = BrCHD0 - kBrCHD * t;
+
+  if BrO3 < 0.001
+	BrO3 = 0.001;
+  endif
+
+  if BrCHD < 0.001
+	BrCHD = 0.001;
+  endif
+
+  xdot = zeros(3, 1);
+
+  galpha = k2 * k14 * BrCHD / (k17 * k17 * BrO3 * BrO3);
+  # beta has no dependency on [BrO3-] or [BrCHD]
+  ggamma = kdp * realsqrt(k14) * realsqrt(BrCHD) / (realpow(k17, 3/2) * realsqrt(H) * BrO3);
+  gdelta = 2 * k5 * k4 * k14 * BrCHD / (k4r * k17 * k17 * BrO3 * BrO3);
+
+  # printf("a = %f\n", galpha);
+  # printf("b = %f\n", gbeta);
+  # printf("g = %f\n", ggamma);
+  # printf("d = %f\n", gdelta);
+
+  xdot(1) = x(2) * (gbeta - galpha * x(1)) + x(3) * (ggamma * realsqrt(x(1)) + 1.0) - gdelta * x(1) * x(1);
+  xdot(2) = 1.0 - x(2) * (gbeta + galpha * x(1));
+  xdot(3) = gf - x(3) * (ggamma * realsqrt(x(1)) + 1.0); 
+
+endfunction
+
+function r = axz(x)
+  global BrO30 kBrO3;
+
+  r = BrO30 - kBrO3 * x;
+
+  if r < 0.001
+	r = 0.001;
+  endif
+endfunction
+
+function r = bxz(x)
+  global BrCHD0 kBrCHD;
+
+  r = BrCHD0 - kBrCHD * x; 
+
+  if r < 0.001
+	r = 0.001;
+  endif
+endfunction
+
+
+# Initial conditions
+#1 HBrO2
+x1 = 0.0007245927973977255 ;
+#2 Br-
+x2 = 0.001422769822454512 ;
+#3 H2Q
+x3 = 0.03193424249332828 ;
+
+x0 = [x1; x2; x3];
+
+# time = kf*t
+#t = linspace (0, 1, 10000);
+#t = linspace (0, 5, 10000);
+t = linspace (0, 15, 15000);
+#t = linspace (0, 1, 1000);
+
+y = lsode ("f", x0, t);
+
+xsteps = 15000;
+#xsteps = 1000;
+xmax = 15;
+#xmax = 1;
+xh = xmax/xsteps;
+xi = 1;
+
+for i=1:xsteps
+  xx = xh * i;
+  xxi(xi) = xx;
+  null_x(xi) = axz(xx);
+  null_z(xi) = bxz(xx);
+  xi = xi + 1;
+end
+
+
+plot (t, y);
+
+xlabel("\\tau");
+ylabel("concentration");
+title(cstrcat(
+			  "SKG4 model: ",
+			  " f=", num2str(gf),
+			  " [BrO_3^-]_0=", num2str(BrO30),
+			  " [BrCHD]_0=", num2str(BrCHD0),
+			  " k_{BrO3}=", num2str(kBrO3),
+			  " k_{BrCHD}=", num2str(kBrCHD)
+			  )
+	  );
+legend("[HBrO_2]", "[Br^-]", "[H_2Q]");
+
+print -djpg skg4.jpg
+
+# E9
+function r = axz(x)
+  global BrO30 kBrO3;
+
+  r = BrO30 - kBrO3 * x;
+
+  if r < 0.001
+	r = 0.001;
+  endif
+endfunction
+
+function r = bxz(x)
+  global BrCHD0 kBrCHD;
+
+  r = BrCHD0 - kBrCHD * x; 
+
+  if r < 0.001
+	r = 0.001;
+  endif
+endfunction
+
+
+
+newplot();
+
+hold on;
+#plot(y(:,1),y(:,3), "color", "black");
+plot(xxi, null_x, "color", "blue");
+plot(xxi, null_z, "color", "red");
+
+#xlim([0, 0.015]);
+#ylim([0, 0.04]);
+
+xlabel("\\tau");
+ylabel("concentration");
+title(cstrcat(
+			  "SKG4 model: ",
+			  " f=", num2str(gf),
+			  " [BrO_3^-]_0=", num2str(BrO30),
+			  " [BrCHD]_0=", num2str(BrCHD0),
+			  " k_{BrO3}=", num2str(kBrO3),
+			  " k_{BrCHD}=", num2str(kBrCHD)
+			  )
+	  );
+
+legend("[BrO_3^-]", "[BrCHD]");
+
+print -djpg skg4x.jpg
+
+d = [rot90(t, -1), y, rot90(null_x, -1), rot90(null_z, -1)];
+
+save plot.mat d;
+
+
+quit(0);
+
+
+
