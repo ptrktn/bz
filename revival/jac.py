b17d2867aecfe84eac0f44b099d0e63d7cd94e8d
Petteri Kettunen <ptrktn@gmail.com>
2019-07-15 10:43:05 +0900

    updates

--- revival/jac.py
+++ revival/jac.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def main(fname=None):
+def main(fname):
     
     vars = ("fkinet", "rkinet", "kf", "kr")
     nvars = 19
@@ -16,37 +16,31 @@ def main(fname=None):
             
     i = "xdot"
     for j in range(1, nx + 1):
-        #exec("%s%d = Function(\"%s%d\")" % (i, j, i, j))
         print("%s%d = Function(\"%s%d\")" % (i, j, i, j))
         print("x%d = Symbol(\"x%d\")" % (j, j))
 
         
-    #try:
     f = open(fname)
     while True:
         line = f.readline().rstrip()
         if not line:
             break
         print(line)
-        #exec(line)
-        
-    #except:
-    #    pass
-    #finally:
     f.close()
 
-    print("print(\" 1; \")")
+    print("print(\"# -*-octave-*-\\n\")")
+    print("print(\" 1; \\n\")")
     print("print(\"function j = jac (x, t)\")")
     print("print(\"    global kf kr ;\")")
-    print("print(\"    j(17, 17) = zeros(17, 17);\")")
+    print("print(\"    j = zeros(17, 17) ;\\n\")")
     
     for i in range(1, nx + 1):
         for j in range(1, nx + 1):
+            print("print(\"    # xdot(%d) = \")," % i)
+            print("print(xdot%d)" % i)
             print("print(\"    j(%d, %d) = \")," % (i, j))
-            #%s ; " % diff(xdot1, x17))
-            #print("j(%d, %d) = ", )
             print("print(diff(xdot%d, x%d))," % (i, j))
-            print("print(\" ; \")")
+            print("print(\" ; \\n\")")
 
 
     print("print(\"endfunction\")")
@@ -54,6 +48,5 @@ def main(fname=None):
         
 if "__main__" == __name__:
     main(sys.argv[1])
-    #main()
 
    
