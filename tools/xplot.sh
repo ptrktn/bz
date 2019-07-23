358d4b8f16efec432aef5187076800ccde461e14
Petteri Kettunen <ptrktn@gmail.com>
2019-07-23 13:35:47 +0900

    create plots in parallel

--- tools/xplot.sh
+++ tools/xplot.sh
@@ -15,14 +15,25 @@ test -f "$FNAME" || exit 1
 NCOLS=$(egrep -v '^#' $FNAME | head -1 | awk '{ print NF }')
 ID=$(echo $(basename $FNAME) | sed 's/\..*$//')_$(date +%Y%m%d%H%M%S)_$(printf "%05d" $RANDOM)
 
+xplot() {
+	local fname=$1
+	local col=$2
+	local j=$(( $col -1 ))
+	local tmp=$(mktemp --tmpdir=/tmp tmp.XXXXX.ps)
+	local f=${ID}_x$(printf "%02d" $j).$FMT
+
+	echo "set term postscript ; set output '$tmp' ; plot '$fname' u 1:$col w l title '${x}${j}'" | gnuplot > /dev/null 2>&1 || exit 1
+	convert -density 300 $tmp -flatten -background white -resize 1024 -rotate 90 $f
+	rm -f $tmp
+}
+
 for i in $(seq 2 $NCOLS) ; do
 	rm -f tmp.ps
 	j=$(( $i -1 ))
-	f=${ID}_x$(printf "%02d" $j).$FMT
-	echo "set term postscript ; set output 'tmp.ps' ; plot '$FNAME' u 1:$i w l title '${x}${j}'" | gnuplot > /dev/null 2>&1 || exit 1
-	convert -density 300 tmp.ps -flatten -background white -resize 1024 -rotate 90 $f
-	rm -f tmp.ps
+	xplot $FNAME $i &
 done
+
+wait
 
 exit 0
 
